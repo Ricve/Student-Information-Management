13cb90eaab53dc39e043e65eea0dd86072045193
Ricve <ricardo172@126.com>
2021-06-19 12:45:10 +0800

    tested find and delete with read and save

--- src/assignment/IOTest.java
+++ src/assignment/IOTest.java
@@ -13,47 +13,10 @@ public class IOTest {
 //		inf.addStudent(new Students("John",13,"nowhere","1234"));
 //		inf.addStudent(new Students("Eric",14,"nowhere","1235"));
 //		inf.addStudent(new Students("Eric",14,"nowhere","1236"));
-		
-		
-		File file =new File("src/data.txt");
-		FileReader fileReader=null;
-		FileWriter fileWriter=null;
-		BufferedWriter bw=null;
-		try {
-			/*∂¡»Î*/
-			if(!file.exists())
-				file.createNewFile();
-			fileReader=new FileReader(file);
-			Scanner in=new Scanner(fileReader);
-			while(in.hasNext()) {
-				inf.addStudent(new Students(in.next(),in.nextInt(),in.next(),in.next()));
-//				System.out.println(in.nextLine());
-			}
-			System.out.println(inf.stuset.size());
-//			System.out.println("here");
-			/*–¥ªÿ*/
-			fileWriter=new FileWriter("src/output.txt");
-			bw=new BufferedWriter(fileWriter);
-			Students student=null;
-			Iterator<Students> it= inf.stuset.iterator();
-			while (it.hasNext()) {
-				student=it.next();
-				bw.write(student.getName());
-				bw.write(" ");
-				bw.write(Integer.toString(student.getAge()));
-				bw.write(" ");
-				bw.write(student.getNativePlace()); 
-				bw.write(" ");
-				bw.write(student.getTelNum());
-				bw.newLine();
-			}
-			bw.flush();
-			
-			in.close();
-			bw.close();
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+//		readFromTxt(inf);
+//		saveAsTxt(inf);
+		inf.readFromTxt();
+		inf.saveAsTxt();
 
 	}
 
